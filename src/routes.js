4d1f7df593f58c2d501a1d15ad4cc9712aa97622
Antonio Sampaio De Vuono <antoniosvuono@gmail.com>
2021-02-19 13:12:53 -0300

    Metódo update, precisa ser corrigido!

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,6 @@ const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
-routes.put('/users', UserController.update);
 
 routes.use(authMiddleware);
 
